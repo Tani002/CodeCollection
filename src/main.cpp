5faf3f7c360a8ae92a894e475db5a7fd075dce76
Tani Sandoval <72883511+Tani002@users.noreply.github.com>
2021-07-10 16:24:34 +0800

    Reformat Document Added viewRecord Edited Options Logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
 #include <cstdlib>
-#include <iomanip>
 #include <fstream>
+#include <iomanip>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
-struct Patients{
+struct Patients
+{
     string name;
-    string age; 
-    string address; 
-    string disease; 
+    string age;
+    string address;
+    string disease;
     string prevHosp;
 };
 
@@ -21,14 +22,11 @@ void deleteRecord();
 
 int main()
 {
-
     int opt1;
-
     cout << "Medical Records Management System" << endl;
     cout << "===============================" << endl;
     cout << "Select an option below: " << endl
          << endl;
-
     cout << "1) Create Medical Record" << endl;
     cout << "2) Edit Medical Record" << endl;
     cout << "3) View Medical Record" << endl;
@@ -41,39 +39,43 @@ int main()
 
     switch (opt1)
     {
-        /*Add Patient Records*/
-        case 1:
-        {
-            addRecord();
-            break;
-        }
-        /*Edit Patient Records*/
-        case 2:
-        {
-            break;
-        }
-        /*View Patient Records*/
-        case 3:
-        {
-            break;
-        }
-        /*Delete Patient Records*/
-        case 4:
-        {
-            break;
-        }
-        /*Program is KIL :(*/
-        case 5:
-        {
-            return 0;
-            break;
-        }
-        /*Failed Input Option*/
-        default:
-        {
-            cout << "Invalid Option" << endl;      
-            break;
-        }
+    //! Add Patient Records
+    case 1:
+    {
+        addRecord();
+        system("cls");
+        return main();
+    }
+    //! Edit Patient Records
+    case 2:
+    {
+        break;
+    }
+    //! View Patient Records
+    case 3:
+    {
+        viewRecord();
+        system("cls");
+        return main();
+    }
+    //! Delete Patient Records
+    case 4:
+    {
+        break;
+    }
+    //! Program is KIL :(
+    case 5:
+    {
+        break;
+    }
+    //! Failed Input Option
+    default:
+    {
+        cout << "Invalid Option" << endl;
+        system("pause");
+        system("cls");
+        return main();
+    }
     }
     return 0;
 }
@@ -82,7 +84,7 @@ void addRecord()
 {
     Patients patient[10];
     char check = 'n';
-    int i=0;
+    int i = 0;
 
     do
     {
@@ -90,7 +92,7 @@ void addRecord()
         cout << endl;
         cout << "Patient Number: " << i << endl;
         cin.ignore();
-        
+
         cout << "Enter Patient Name: ";
         getline(cin, patient[i].name);
         cout << "Enter Patient Address: ";
@@ -101,17 +103,50 @@ void addRecord()
         getline(cin, patient[i].prevHosp);
         cout << "Would you like to input another record? [y/n] ";
         cin >> check;
-
-    }while(check == 'y' ||check == 'Y');
+    } while (check == 'y' || check == 'Y');
 
     ofstream file;
     file.open("patients.csv");
     file << "Patient Number,Patient Name,Address,Disease,Previous Hospital\n";
-    int ctr=0;
+    int ctr = 0;
     do
     {
         ctr++;
         file << ctr << "," << patient[ctr].name + "," << patient[ctr].address + "," << patient[ctr].disease + "," << patient[ctr].prevHosp << "\n";
-    }while (ctr < i);
+    } while (ctr < i);
     file.close();
+}
+
+void viewRecord()
+{
+    ifstream myReadFile("patients.csv");
+    string TableNames;
+    string Number;
+    string Name;
+    string Address;
+    string Disease;
+    string PrevHosp;
+    int x = 0;
+    while (myReadFile.good())
+    {
+        if (x < 1)
+        {
+            getline(myReadFile, TableNames, '\n');
+            x++;
+        }
+        getline(myReadFile, Number, ',');
+        getline(myReadFile, Name, ',');
+        getline(myReadFile, Address, ',');
+        getline(myReadFile, Disease, ',');
+        getline(myReadFile, PrevHosp, '\n');
+
+        cout << "Patient Number: " << Number << '\n';
+        cout << "Patient Name: " << Name << '\n';
+        cout << "Patient Address: " << Address << '\n';
+        cout << "Patient Diseas: " << Disease << '\n';
+        cout << "Patient Previous Hospital: " << PrevHosp << '\n'
+             << endl;
+    }
+    myReadFile.close();
+    system("pause");
 }
